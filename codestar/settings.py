79b84ee299d8fa19d28300cb1dd2445fa37544b7
patriklundstrom91 <patriklundstrom91@gmail.com>
2025-05-07 11:14:02 +0200

    install gunicorn and prepare for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_5-_71p713t@%ey#s=#c+6!rryhnv4lw3)^tq(km_-wh-lv93%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
