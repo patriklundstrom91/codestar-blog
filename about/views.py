ab42cc69cdc9819c85092eeab9a667308b076c86
patriklundstrom91 <patriklundstrom91@gmail.com>
2025-05-16 22:05:17 +0200

    code fixes

--- about/views.py
+++ about/views.py
@@ -4,9 +4,19 @@ from .models import About
 from .forms import CollaborateForm
 # Create your views here.
 
+
 def about_me(request):
     """
-    Renders the about page
+    Renders the most recent info on the website author
+    and allows user collaboration requests
+    Displays an individual instance of :model: `about.About`.
+    **Context**
+     ``about``
+        The most recent instance of :model: `about.About`.
+     ``collaborate_form``
+        An instance of :form: `about.CollaborateForm`.
+    **Template**
+    :template: `about/about.html`
     """
     about = About.objects.all().order_by('-updated_on').first()
     if request.method == "POST":
--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
+
+
 class Post(models.Model):
+    """
+    Stores a single blog post entry related to :model: `auth.User`.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -25,6 +31,10 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+    """
+    Stores a single comment entry realated to :model: `auth.User`
+    and :model: `blog.Post`
+    """
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments"
     )
