18365527882ae08a95090141856d6c7449f699c9
patriklundstrom91 <patriklundstrom91@gmail.com>
2025-05-16 11:28:51 +0200

    add collab request and wired it up

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 # Create your views here.
@@ -8,6 +9,14 @@ def about_me(request):
     Renders the about page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collab request received! I endeavor to respond within 2 working days.'
+                )
     collaborate_form = CollaborateForm()
 
     return render(
